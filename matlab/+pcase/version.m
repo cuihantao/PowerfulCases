ea89fb57545b610346316d9f3f4f360443f67fdf
Hantao Cui <cuihantao@gmail.com>
2025-12-29 14:46:12 +0800

    Update version to 0.4.0 in Project.toml and pyproject.toml; add MATLAB version function

---
+++ matlab/+pcase/version.m
@@ -0,0 +1,10 @@
+function v = version()
+%VERSION Return the version of PowerfulCases MATLAB API
+%   v = pcase.version() returns the version string.
+%
+%   Example:
+%       v = pcase.version();
+%       disp(v)  % "0.4.0"
+
+    v = '0.4.0';
+end
